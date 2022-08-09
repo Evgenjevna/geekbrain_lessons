e8cfcb4f455ba31ccf3d71a15cb2f1d963b2ccdd
Elizaveta <evgenjevna25@gmail.com>
2022-08-09 14:33:48 +0300

    .\example_012\

---
+++ example_012/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int numberC = (numberB % numberA);
+if (numberC == 0)
+{
+    Write($"кратно: {numberC}");
+}
+else
+{
+    Write($"не кратно: {numberC}");
+}
