02a2e437da6fed31b31d23505779f4b38573ca66
Elizaveta <evgenjevna25@gmail.com>
2022-08-09 14:31:08 +0300

    .\example_011\

---
+++ example_011/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+Clear();
+int randomNumber = new Random().Next(100,999);
+WriteLine(randomNumber);
+int result = randomNumber / 100;
+int thirdDigit = randomNumber %10;
+Write(result);
+Write(thirdDigit);
