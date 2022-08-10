fb65ad678bdc9373788058846b59d7210082ff46
Elizaveta <evgenjevna25@gmail.com>
2022-08-10 21:19:55 +0300

    .\HomeWork_task13 2 типа решения"

---
+++ HomeWork_task13/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число");
+//Решение задачи через строку
+//string UsersInput = ReadLine();
+//int n = UsersInput.Length;
+//if (n >= 3)
+//{
+//    WriteLine(UsersInput[2]);
+//}
+//else
+//{
+//    WriteLine("Число имеет меньше цифр");
+//};
+
+//Математическое решение решение
+int n = int.Parse(ReadLine());
+while (n > 999)
+    n = n / 10;
+int result = n % 10;
+if (n > 99)
+{
+    WriteLine(result);
+}
+else
+{
+    WriteLine("Число имеет меньше цифр");
+}
