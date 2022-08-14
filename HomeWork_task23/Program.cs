7f891c92ac0d028b16a1e1e99d03c2d7153ba6d9
Elizaveta <evgenjevna25@gmail.com>
2022-08-14 23:49:41 +0300

    таблица кубов от 1 до N

---
+++ HomeWork_task23/Program.cs
@@ -0,0 +1,9 @@
+﻿//Программа принимает на вход число и выдаёт таблицу кубов от 1 до N
+using static System.Console;
+Clear();
+WriteLine("Введте число N");
+int digit=int.Parse(Console.ReadLine());
+for (int i = 1; i <= digit; i++)
+{
+    Write($"{i * i * i} ");
+}
