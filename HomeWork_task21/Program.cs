798492f44bf39a869c1ee9a90e787708706e2c48
Elizaveta <evgenjevna25@gmail.com>
2022-08-14 23:32:00 +0300

    Home Work Task21

---
+++ HomeWork_task21/Program.cs
@@ -0,0 +1,20 @@
+﻿//Нахождение расстояния между двумя точками в 3D пространстве
+using static System.Console;
+Clear();
+WriteLine("Введите координаты точки А ");
+int x1 = Convert.ToInt32(ReadLine());
+int y1 = Convert.ToInt32(ReadLine()); 
+int z1 = Convert.ToInt32(ReadLine());
+
+WriteLine("Введите координаты точки B ");
+int x2 = Convert.ToInt32(ReadLine());
+int y2 = Convert.ToInt32(ReadLine());
+int z2 = Convert.ToInt32(ReadLine());
+
+int a = x2 - x1;
+int b = y2 - y1;
+int c = z2 - z1;
+
+int d = a * a + b * b + c *c;
+double result = Math.Sqrt(d);
+WriteLine($"Отрезок равен {result:f2}");
