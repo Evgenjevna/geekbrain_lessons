c71dfd49b5525d2624408eb796070168dd44f720
Elizaveta <evgenjevna25@gmail.com>
2022-08-19 22:40:35 +0300

    Рандомный массив из 8 элементов с подсчётом количества чётных элементов

---
+++ HomeWork_task34/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+using static System.Console;
+Clear();
+int[] even = GetArray(8, 100, 999);
+WriteLine($"Массив: [{String.Join(",", even)}]");
+int N = 0;
+for (int i = 0; i < even.Length; i++){
+    if (even[i] % 2 == 0)
+        N+=1;
+}
+WriteLine($"Количество чётных элементов в массиве: {N}");
+
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
