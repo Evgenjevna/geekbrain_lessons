d9edb8a96d348c778aa37319f1b831d04b5bddce
Elizaveta <evgenjevna25@gmail.com>
2022-08-20 22:12:13 +0300

    Подсчёт суммы нечётных элементов массива

---
+++ HomeWork_task36/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+using static System.Console;
+Clear();
+int[] even = GetArray(15, -100, 100);
+WriteLine($"Массив: [{String.Join(",", even)}]");
+int N = 0;
+for (int i = 0; i < even.Length; i++){
+    if (i % 2 == 0)
+        N=N+even[i];
+}
+WriteLine($"Сумма элементов стоящих на нечётных позициях в массиве: {N}");
+
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
