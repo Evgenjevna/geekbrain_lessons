6b4693844199e003b5a25d13aefd7eada7be0be7
Elizaveta <evgenjevna25@gmail.com>
2022-08-25 12:09:05 +0300

    Нахождение количества положительных чисел в массиве введённом с клавиатуры

---
+++ HomeWork_task41/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+using System.Linq;
+Clear();
+WriteLine("Введите числа через пробел:");
+int count = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(M=>int.Parse(M)).ToArray().Where(a=>a>0).Count();
+WriteLine(count);
